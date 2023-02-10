1b0ef0ae09d71145417361ca710c234ef480ab04
jerryliang <jerryliang@jerryliang.top>
2023-02-10 18:00:53 +0800

    支持图片转换

--- email/inquiry/smtp.py
+++ email/inquiry/smtp.py
@@ -2,5 +2,11 @@ import smtplib
 import base64
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import os
+
 
 # 处理放置在conf文件中的签名照片
+def get_img():
+    with open(os.path.join(os.getcwd(), "conf", "signature"), "rb") as f:
+        img_data = f.read()
+    return img_data
